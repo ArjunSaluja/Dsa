460eecac2203f292e1d9ead7433af8ce3d2f67ed
Arjun Saluja <arjunsaluja2004@gmail.com>
2025-01-23 23:40:42 +0530

    cpp

---
+++ basic.cpp
@@ -0,0 +1,59 @@
+#include<iostream>
+ using namespace std;
+ int main(){
+    //  int a =123;
+    //  cout<< a <<endl;
+
+    //  char b ='v';
+    //  cout<<b <<endl;
+
+    //  bool bl = true;
+    //  cout<<bl<<endl;
+
+    //  float fl = 12.3;
+    //  cout<<fl<<endl;
+
+    //  double db = 12.22;
+    //  cout<<db<<endl;
+
+    //  int size = sizeof(a);
+    //  cout<<"Size of a is :"<<size<<endl;
+
+
+
+    //  int c = 'a';
+    //  cout<<a<<endl;// typecasting
+
+    //  char ch = 98;
+    //  cout<< ch<<endl;
+      
+
+    // // agar jada valuee ka daldiya to kya hoga
+
+     char ch2= 123456;
+    cout<< ch2<< endl;
+
+    unsigned int r =-1234445;
+    cout<<r<<endl;
+
+    int a1 =2/5;
+    cout<<a1<<endl;
+
+    float  b1=2.0/5;
+  cout<<b1<<endl;
+
+  int w = 2;
+  int q= 3;
+   bool o =(w==q);
+   cout<<o<<endl;
+
+ int a= 0;
+ cout<<!a<<endl;
+
+ int b=223;
+ cout<<!b<<endl;
+
+ 
+
+
+ }
