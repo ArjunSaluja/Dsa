1059746bef6c7e12e987b117e20b74904cfdfd64
Arjun Saluja <arjunsaluja2004@gmail.com>
2025-01-24 22:58:17 +0530

    cpp

---
+++ counting1121.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+void print(int n){
+    if(n==0){
+        return ;
+
+}
+cout<<n<<endl;
+//recursive relation or agar cout<<n ko print n-1 ka baad likaga to count 1 2 3 4 5
+print(n-1);
+}
+int main(){
+    int n;
+    cin>>n;
+    print(n);
+
+    return 0;
+}
