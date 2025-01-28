d6dbfd1ee12c55d10a1902a37a3571775f114b77
Arjun Saluja <arjunsaluja2004@gmail.com>
2025-01-28 23:01:15 +0530

    cpp

---
+++ print.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+ using namespace std;
+ int main(){
+
+// int n;
+// cin>>n;
+// //cout<<"hello"<<endl;
+
+// if (n>0)
+// {
+//     cout<<"N is Positive"<<endl;
+// }
+// else{
+//     cout<<"N is Negative"<<endl;
+// }
+
+ int a, b;
+// cin>>a>>b;
+
+//cout<<"value of a and b is "<<a <<" "<<b <<endl;
+cout<<"Enter the value of A:"<<endl;
+cin>>a;
+cout<<"Enter the value of B:"<<endl;
+cin>>b;
+if(a>b){
+    cout<<"A is greater"<<endl;
+}
+if(b>a){
+    cout<<"B is greater"<<endl;
+}
+
+ }
