b9212ce0b5a3745e1bc0c6182466776ce5e19399
Arjun Saluja <arjunsaluja2004@gmail.com>
2025-02-08 23:25:40 +0530

    cpp

---
+++ char.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+ using namespace std;
+ int main(){
+
+    char ch ='1';
+
+    cout<< endl;
+     switch (ch)
+     {
+     case 1: cout<<" fi<rst"<<endl;
+     break;
+     case '1':cout<< "character one"<< endl;
+     break;
+
+     default: cout<< "it is default case"<<endl;
+ }
+ cout<<endl;
+ return 0;
+ }
