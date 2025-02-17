47f3c98f3dd238fbfb51f5068b0a9b8e76616266
Arjun Saluja <arjunsaluja2004@gmail.com>
2025-02-17 22:33:26 +0530

    cpp

---
+++ fuction.cpp
@@ -0,0 +1,23 @@
+//////////////// FUNCTION //////////////////////////////
+// function is a program that is well defined
+#include<iostream>
+ using namespace std;
+ int power(int a, int b){
+    int ans =1;
+    for (int i = 1; i <=b; i++)
+    {
+        ans= ans*a;
+    }
+    return ans;
+    
+ }
+ int main()
+{
+  int a,b;
+  cin>>a>>b;
+
+  int answer= power(a,b);
+  cout<<"answerr is"<< answer<<endl;
+  
+  return 0;
+}
