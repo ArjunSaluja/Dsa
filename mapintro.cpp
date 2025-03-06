973160886722b3a519956b37e7d6778b6cf17561
Arjun Saluja <arjunsaluja2004@gmail.com>
2025-03-06 13:33:13 +0530

    cpp

---
+++ mapintro.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+#include<map>
+#include<unordered_map>
+using namespace std;
+
+int main(){
+    // creation
+    unordered_map<string, int> m;
+    //insertion 1
+    pair<string,int> p = make_pair("babbar", 3);
+    m.insert(p);
+   
+   // insert2
+   pair<string , int> pair2("love", 2);
+   m.insert(pair2);
+
+   // 3 single entry no duplicate key
+
+   m["mera"]=1;
+
+   // what will happen 
+   m["mera"]=2;
+
+   // search
+   cout<<m["mera"]<<endl;
+   cout<<m.at("babbar")<<endl;
+
+   cout<<m.at("unknownkey")<<endl;
+
+   // size()
+   cout<<m.size()<<endl;
+
+   // check pressence
+   cout<<m.count("bro")<<endl;
+
+   //erase
+   m.erase("love");
+   cout<<m.size()<<endl;
+   
+
+
+}
